ae4a3a8a16bd7eb878d07e0a6f44d7ae8a58012f
daiki.ikuta <amaotono1902@gmail.com>
2020-10-09 02:32:46 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by daiki ikuta on 2020/10/09.
+//  Copyright © 2020 daiki.ikuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
